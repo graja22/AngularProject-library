bdf0555abeecdf50dd3f8c09fb1b3fb033a2950e
gouthamraja22 <gouthamraja22@gmail.com>
2019-08-01 17:23:46 +0530

    Auto

--- auto.bat
+++ auto.bat
@@ -2,5 +2,5 @@
 git add .
 git commit -m "Auto"
 git push -f
-ng build --prod --base-href "https://graja22.github.io/library"
+ng build --prod --base-href "https://graja22.github.io/library/"
 ngh --dir dist/bookT --no-silent
